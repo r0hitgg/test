122aee93ac391866d3400ad9cf97b8eadf5bfeda
Hiren Jungi <hiren@techreceptives.com>
2020-01-07 15:11:26 +0530

    let you decide how you want to separate

--- comma_saperator.py
+++ comma_saperator.py
@@ -1,6 +1,9 @@
-inp = input()
-inp1 = int(input())
-print("number with coma " + "{:,}".format(inp1))
-inp = [int(i) for i in inp]
-for i in inp:
-    print(i,end=',')
+i = int(input('''choose 1 for seprate every character\n 2 for seprate with comma in amount format'''))
+if i == 1:
+    inp1 = int(input())
+    print("number with coma " + "{:,}".format(inp1))
+if i == 2:
+    inp = input()
+    inp = [int(i) for i in inp]
+    for i in inp:
+        print(i,end=',')
